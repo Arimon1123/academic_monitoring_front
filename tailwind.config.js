dcea5aab5a126b783aac1bb39b8e52f680f71ebb
ArielMonroy <ariel.monroy@ucb.edu.bo>
2024-02-14 15:13:07 -0400

    added flowbite plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,23 +9,11 @@ module.exports = {
       colors: {
         "bg-color": "#F9F9F9",
         "primary": "#0C2348",
-        "active": "#159DFF"
+        "active": "#159DFF",
+        "selected": "#438ABE" 
       },
       screens: {
-        '2xl': { 'max': '1535px' },
-        // => @media (max-width: 1535px) { ... }
-
-        'xl': { 'max': '1279px' },
-        // => @media (max-width: 1279px) { ... }
-
-        'lg': { 'max': '1023px' },
-        // => @media (max-width: 1023px) { ... }
-
-        'md': { 'max': '767px' },
-        // => @media (max-width: 767px) { ... }
-
-        'sm': { 'max': '639px' },
-        // => @media (max-width: 639px) { ... }
+       
       },
       gridTemplateColumns: {
         '2A': '3fr 1fr',
